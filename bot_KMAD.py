10c653e536e82697fbd9af3a5b130c1bf9e76a54
Valentyna Pikalova <vpikalova@hotmail.com>
2023-02-15 02:45:32 +0200

    Update bot_KMAD.py

--- bot_KMAD.py
+++ bot_KMAD.py
@@ -396,7 +396,8 @@ def kilkistMists(update: Update, context: CallbackContext):
 
 
 def question(update, context):
-    update.callback_query.message.reply_text('[__Напишіть нашому представнику кафедри(тапні для переходу)__](https://t.me/kmad_khpi/)',
+    update.callback_query.message.reply_text('[__Напишіть нашому представнику кафедри (тапнИ для переходу) або приєднайтеся до Інфрмаційної групи для абітурієнтів  кафедри, 
+де зможете отримати відповіді на всі запитання, які вас цікавлять (https://t.me/+mpwNrmMVtzMzZmNi )__](https://t.me/+mpwNrmMVtzMzZmNi)',
                                              parse_mode="Markdown", disable_web_page_preview=True)
 
 
