f15dec44dc8aa0f6d78b1c6abe61098a2ad7c631
Valentyna Pikalova <vpikalova@hotmail.com>
2023-02-15 12:33:41 +0200

    Update bot_KMAD.py

--- bot_KMAD.py
+++ bot_KMAD.py
@@ -225,22 +225,17 @@ def istoria(update: Update, context: CallbackContext):
     photos = read_content(link +
                           contents['start']['next_menu']['kafedra']['next_menu']['istoria']['photo'][0])
     
-    photos = '''images/k_i_01.jpg
-    images/k_i_02.jpg
-    images/k_i_03.jpg
-    images/k_i_04.jpg
-    images/k_i_05.jpg
-    images/k_i_06.jpg
-    images/k_i_07.jpg
-    images/k_i_08.jpg
-    images/k_i_09.jpg
-    images/k_i_10.jpg
-    images/k_i_11.jpg
-    images/k_i_12.jpg
-    images/k_i_13.jpg
-    images/k_i_14.jpg
-    images/k_i_15.jpg'''
-    photos = sorted(photos.split()[5:])
+    photos = '''images/k_i_0.jpg
+    images/k_i_1.jpg
+    images/k_i_2.jpg
+    images/k_i_3.jpg
+    images/k_i_4.jpg
+    images/k_i_5.jpg
+    images/k_i_6.jpg
+    images/k_i_7.jpg
+    images/k_i_8.jpg
+    images/k_i_9.jpg'''
+    photos = sorted(photos.split())
     query = update.callback_query
     #query.answer()
     content_list = content.split('||')
