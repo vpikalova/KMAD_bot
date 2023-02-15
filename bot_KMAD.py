06365ff2bffd143633b020fe0254c7d962ca4ad8
Valentyna Pikalova <vpikalova@hotmail.com>
2023-02-15 11:10:07 +0200

    Update bot_KMAD.py

--- bot_KMAD.py
+++ bot_KMAD.py
@@ -235,6 +235,7 @@ def istoria(update: Update, context: CallbackContext):
         text_message = content_list[i]
         query.message.reply_text(text= text_message, parse_mode="Markdown")
         link_photo = link + photos[i]
+        print(link_photo)
         query.message.reply_photo(link_photo)
     query.message.reply_text(text=content_list[-1], reply_markup=reply, parse_mode="Markdown")
 
